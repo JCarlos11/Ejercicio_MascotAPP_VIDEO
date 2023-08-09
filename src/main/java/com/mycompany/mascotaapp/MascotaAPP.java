ad8a75d78ed58fbcbf7ce4bcfae9ede85c0a430e
JCarlos11 <jocarlos256@gmail.com>
2023-08-08 18:45:08 -0600

    Mapas

--- src/main/java/com/mycompany/mascotaapp/MascotaAPP.java
+++ src/main/java/com/mycompany/mascotaapp/MascotaAPP.java
@@ -6,7 +6,9 @@ package com.mycompany.mascotaapp;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 import mascotapp.utilidades.Comparadores;
@@ -21,32 +23,45 @@ public class MascotaAPP {
 
     public static void main(String[] args) {
         
+        HashMap<String, Mascota> mascotas = new HashMap();
         
-        TreeSet<Mascota> mascotas = new TreeSet();
+        mascotas.put("1" , new Mascota("Fer", "Chiquito", "Perro"));
+        mascotas.put("2", new Mascota("Pepa", "Lola", "Gato"));
         
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
-        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
-        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
-        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
-        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
-        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+        mascotas.remove("3");
         
+        for (Map.Entry<String, Mascota> aux : mascotas.entrySet()) {
+            String key = aux.getKey();
+            Mascota value = aux.getValue();
+            
+            System.out.println("key " + key + "value " + value);
+        }
+        
+//        TreeSet<Mascota> mascotas = new TreeSet();
+//        
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
+//        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
+//        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
+//        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
+//        mascotas.add(new Mascota("Lola", "Gato", "Pepa"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        mascotas.add(new Mascota("Chiquito", "Perro", "Fer"));
+//        
 //        Collections.sort(mascotas,Comparadores.ordenarPorNombreDesc);
 //        
 //        Collections.shuffle(mascotas);
 //        
-        for (Mascota mascota : mascotas) {
-            System.out.println(mascota);
-        }
-        
+//        for (Mascota mascota : mascotas) {
+//            System.out.println(mascota);
+//        }
+//        
 //        ServicioMascota servMasc = new ServicioMascota();
 //        
 //        servMasc.fabricaMascota(2);
